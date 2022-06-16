d5183e344083cf98338186ef9990572eb04398a1
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-16 09:22:55 -0300

    Removed libraries

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import psycopg2
 import os
 from flask import Flask, render_template
-from module.postg import convert_postgres_url_to_dict
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 app = Flask(__name__)
