7a26bf1837072caa16c62d7f7bc49454d8191b3d
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-16 11:30:26 -0300

    Test sql changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import psycopg2
 import os
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 DATABASE_URL = os.environ.get('DATABASE_URL')
 app = Flask(__name__)
 conn = None
@@ -24,15 +24,21 @@ if conn:
             commit;""")
         
         return "Created 'note' table"
-    @app.route('/sql_insert', methods=["GET"])
+    @app.route('/sql_insert', methods=["POST"])
     def sql_insert():
-        note = request.args.get('note')
-        with conn.cursor() as cursor:
-            cursor.execute(f"INSERT INTO public.note(note_text) VALUES (\'{note}\');commit;")
-        return f'Inserted "{note}"'
-    @app.route('/sql_select')
+        note = request.form.get('note')
+        if(note):
+            with conn.cursor() as cursor:
+                cursor.execute(f"INSERT INTO public.note(note_text) VALUES (\'{note}\') RETURNING note_id;commit;")
+            return redirect(f"/sql_select?note_id={cursor.fetchone()[0]}")
+        else:
+            render_template("sql_insert.html")
+    @app.route('/sql_select', methods=["GET"])
     def sql_select():
+        note_id = request.args.get('note_id')
         with conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM public.note")
+            sql_query = "SELECT * FROM public.note"
+            
+            cursor.execute()
             return str(cursor.fetchall())
 if __name__ == '__main__': app.run(debug=True)
