a33d827a8933b7f5e8293f3c606dba782cbfde33
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-16 10:31:36 -0300

    Test SQL

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
+import psycopg2
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 DATABASE_URL = os.environ.get('DATABASE_URL')
-
 app = Flask(__name__)
+conn = None
+if DATABASE_URL:
+    conn = psycopg2.connect(DATABASE_URL)
 @app.route('/')
 def index():
     return render_template('index.html')
 
+if conn:
+    @app.route('/sql/create')
+    def sql_create():
+        with conn.cursor() as cursor:
+            cursor.execute("""CREATE TABLE IF NOT EXISTS public.note (
+        created_at timestamp without time zone NOT NULL,
+        note_text text NULL,
+        note_id integer NOT NULL
+    );
+    ALTER TABLE
+        public.note
+    ADD
+        CONSTRAINT note_pkey PRIMARY KEY (note_id);
+        """)
+        return True
+    @app.route('/sql/insert', methods=["GET"])
+    def sql_insert():
+        with conn.cursor() as cursor:
+            cursor.execute(f"INSERT INTO note VALUES ({request.args.get('note')})")
+        return True
+    @app.route('/sql/select')
+    def sql_select():
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM note")
+        return cursor.fetchall()
 if __name__ == '__main__': app.run(debug=True)
