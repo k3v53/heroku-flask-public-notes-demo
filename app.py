cc81c8248a637088fdfb5691f99ce65b682a0adc
k3v <25158905+k3v53@users.noreply.github.com>
2022-06-16 10:34:12 -0300

    Added if again

--- app.py
+++ app.py
@@ -10,28 +10,29 @@ if DATABASE_URL:
 def index():
     return render_template('index.html')
 
-@app.route('/sql_create')
-def sql_create():
-    with conn.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS public.note (
-    created_at timestamp without time zone NOT NULL,
-    note_text text NULL,
-    note_id integer NOT NULL
-);
-ALTER TABLE
-    public.note
-ADD
-    CONSTRAINT note_pkey PRIMARY KEY (note_id);
-    """)
-    return True
-@app.route('/sql_insert', methods=["GET"])
-def sql_insert():
-    with conn.cursor() as cursor:
-        cursor.execute(f"INSERT INTO note VALUES ({request.args.get('note')})")
-    return True
-@app.route('/sql_select')
-def sql_select():
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT * FROM note")
-    return cursor.fetchall()
+if conn:
+    @app.route('/sql_create')
+    def sql_create():
+        with conn.cursor() as cursor:
+            cursor.execute("""CREATE TABLE IF NOT EXISTS public.note (
+        created_at timestamp without time zone NOT NULL,
+        note_text text NULL,
+        note_id integer NOT NULL
+    );
+    ALTER TABLE
+        public.note
+    ADD
+        CONSTRAINT note_pkey PRIMARY KEY (note_id);
+        """)
+        return True
+    @app.route('/sql_insert', methods=["GET"])
+    def sql_insert():
+        with conn.cursor() as cursor:
+            cursor.execute(f"INSERT INTO note VALUES ({request.args.get('note')})")
+        return True
+    @app.route('/sql_select')
+    def sql_select():
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM note")
+        return cursor.fetchall()
 if __name__ == '__main__': app.run(debug=True)
